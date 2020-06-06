e35329b78e72e31a3489bef7b84323d1ec6889f4
rzlamrr <rizal82rebel@gmail.com>
2020-06-07 06:56:36 +0700

    alive: improve msg

--- userge/plugins/fun/alive.py
+++ userge/plugins/fun/alive.py
@@ -17,36 +17,31 @@ LOGO_STICKER_ID, LOGO_STICKER_REF = None, None
 @userge.on_cmd("alive", about={'header': "This command is just for fun"})
 async def alive(message: Message):
     await message.delete()
-    try:
-        if LOGO_STICKER_ID:
-            await sendit(LOGO_STICKER_ID, message)
-        else:
-            await refresh_id()
-            await sendit(LOGO_STICKER_ID, message)
-    except (FileIdInvalid, FileReferenceEmpty, BadRequest):
-        await refresh_id()
-        await sendit(LOGO_STICKER_ID, message)
+    #try:
+    #    if LOGO_STICKER_ID:
+    #        await sendit(LOGO_STICKER_ID, message)
+    #    else:
+    #        await refresh_id()
+    #         await sendit(LOGO_STICKER_ID, message)
+    #except (FileIdInvalid, FileReferenceEmpty, BadRequest):
+    #    await refresh_id()
+    #    await sendit(LOGO_STICKER_ID, message)
     output = f"""
-**USERGE is Up and Running**
+**UsergeX is Up and Running**
 
-       __Durable as a Serge__
-
-• **python version** : `{versions.__python_version__}`
-• **pyrogram version** : `{versions.__pyro_version__}`
-• **userge version** : `{get_version()}`
-• **license** : {versions.__license__}
-• **copyright** : {versions.__copyright__}
-• **repo** : [Userge]({Config.UPSTREAM_REPO})
+• **python**   : `v{versions.__python_version__}`
+• **pyrogram** : `v{versions.__pyro_version__}`
+• **userge**   : `X{get_version()}`
 """
     await userge.send_message(message.chat.id, output, disable_web_page_preview=True)
 
 
-async def refresh_id():
-    global LOGO_STICKER_ID, LOGO_STICKER_REF
-    sticker = (await userge.get_messages('theUserge', 8)).sticker
-    LOGO_STICKER_ID = sticker.file_id
-    LOGO_STICKER_REF = sticker.file_ref
+#async def refresh_id():
+#    global LOGO_STICKER_ID, LOGO_STICKER_REF
+#    sticker = (await userge.get_messages('theUserge', 8)).sticker
+#    LOGO_STICKER_ID = sticker.file_id
+#    LOGO_STICKER_REF = sticker.file_ref
 
 
-async def sendit(fileid, message):
-    await userge.send_sticker(message.chat.id, fileid, file_ref=LOGO_STICKER_REF)
+#async def sendit(fileid, message):
+#    await userge.send_sticker(message.chat.id, fileid, file_ref=LOGO_STICKER_REF)
