14b6eb02e110f7e3f369a4a734b98062d68689a3
rzlamrr <rizal82rebel@gmail.com>
2020-06-20 15:18:21 +0700

    map: fixes

--- userge/plugins/misc/map.py
+++ userge/plugins/misc/map.py
@@ -3,7 +3,7 @@
 #Original module from @skittles9823 < https://github.com/skittles9823/SkittBot >
 #Ported to UsergeX by @rzlamrr < https://github.com/rzlamrr/UsergeX >
 
-import requests
+
 from userge import userge, Message
 
 from geopy.geocoders import Nominatim
@@ -13,18 +13,18 @@ from geopy.geocoders import Nominatim
     'header': "Get map of a location",
     'usage': "{tr}map [location]",
     'examples': "{tr}map Jakarta"})
-async def map(message: Message) -> None:
-    text = message.input_or_reply_str
+async def map(message: Message):
+    args = message.input_str
     await message.edit("Using God Eye...")
     try:
         geolocator = Nominatim(user_agent="SkittBot")
-        location = " ".join(text)
+        location = " ".join(args)
         geoloc = geolocator.geocode(location)  
         lon = geoloc.longitude
         lat = geoloc.latitude
         await message.delete()
         await userge.send_location(message.chat.id, lon, lat)
-        output = f"Open {text} with [Google Maps](https://www.google.com/maps/search/{lat},{lon})"
+        output = f"Open {args} with [Google Maps](https://www.google.com/maps/search/{lat},{lon})"
         await userge.send_message(message.chat.id, output, disable_web_page_preview=True)
-    except AttributeError:
-        await message.edit(f"Can't find {text}", del_in=10)
+    except:
+        await message.edit(f"Can't find {args}", del_in=10)
